c5739ea81c63a9f19e79c4dd4da876b32a156b02
Jeremy Mart Valentino Nainggolan <jeremy.nainggolann@gmail.com>
2025-04-29 09:49:44 +0700

    Update AuthController.php

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -9,37 +9,50 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
     public function login(Request $request)
     {
+        date_default_timezone_set('Asia/Jakarta');
         $email = $request->email;
 
         /*
          * For Development only.
          * Uncomment for production.
          * 
-         * 
          * if (!str_ends_with($email, '@airasia.com')) {
-         *    return redirect('/')->with('error', 'Only users with @airasia.com email are allowed.');
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User is not allowed to login.',
+            ], 403);
          * }
+         * 
          */
+
+
 
         $db_user = User::where('email', $email)->first();
 
         if ($db_user) {
             if ($db_user['status'] == 'VALID') {
+                $db_user->tokens()->delete();
+
                 $token = $db_user->createToken('auth_token')->plainTextToken;
-                
+
                 $update = User::where('email', '=', $email)->update([
                     'name' => $request->name,
-                    'photo_url' => $request->photo_url,  
+                    'photo_url' => $request->photo_url,
+                    'last_login' => date("Y-m-d H:i:s"),
+                    'token' => $token,
+                    'token_type' => 'Bearer Token',
                 ]);
+
+                $user = User::where('email', operator: $email)->first();
 
                 return response()->json([
                     'status' => 'success',
                     'message' => 'Login successful.',
                     'data' => [
-                        'token' => $token,
-                        'token_type' => 'Bearer',
+                        'user' => $user,
                     ],
                 ], 200);
             } else {
